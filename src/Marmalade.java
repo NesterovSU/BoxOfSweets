86605b4659164a4f489ae685372fa10a9f680bcb
Sergey Nesterov <nesterov_s_u@mail.ru>
2022-02-22 19:38:29 +0300

    Создан класс Marmalade

---
+++ src/Marmalade.java
@@ -0,0 +1,21 @@
+/**
+ * @author Sergey Nesterov
+ */
+public class Marmalade extends Sweet{
+    private final String COLOR;
+
+    public Marmalade(){
+        super("Мармелад", 250, 125);
+        this.COLOR = "красный";
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.printf("Наименование: %s, цвет: %s,  вес: %d, цена: %d",
+                super.getName(),
+                this.COLOR,
+                super.getWeigth(),
+                super.getCost()
+        );
+    }
+}
